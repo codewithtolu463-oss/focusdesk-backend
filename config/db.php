f0324ea796b6d01ed2ae1cb55a513a76e654a1fd
codewithtolu463-oss <codewithtolu463@gmail.com>
2026-04-30 14:29:26 +1200

    Update db.php

--- config/db.php
+++ config/db.php
@@ -1,9 +1,9 @@
 <?php
-$host = getenv('MYSQLHOST');
-$username = getenv('MYSQLUSER');
-$password = getenv('MYSQLPASSWORD');
-$database_name = getenv('MYSQLDATABASE');
-$port = getenv('MYSQLPORT');
+$host = getenv('mysql.railway.internal');
+$username = getenv('root');
+$password = getenv('fcoOgbHYRYOjdOjlnPkPjULLIwVAUsYu');
+$database_name = getenv('railway');
+$port = getenv('3306');
 $conn = new mysqli($host, $username, $password, $database_name, $port);
 if($conn->connect_error){die(json_encode(['message' => 'Connection failed: ' . $conn->connect_error]));}
 ?>
