3fb08a4fec8af08795f6d023d30a61c45bbe6e7b
codewithtolu463-oss <codewithtolu463@gmail.com>
2026-05-05 10:53:05 +1200

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,25 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 header("Access-Control-Allow-Origin: https://focusdeskk.netlify.app");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header("Content-Type: application/json");
-
+ 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
     exit();
 }
+if ($_SERVER['REQUEST_URI'] === '/test') {
+    echo json_encode(['message' => 'Server is working!']);
+    exit();
+}
+if ($_SERVER['REQUEST_URI'] === '/dbtest') {
+    require_once 'config/db.php';
+    echo json_encode(['message' => 'DB connected!']);
+    exit();
+}
 
 require_once 'routes/api.php';
+
+?>
