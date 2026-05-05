49d26bdb4164457ca719461399da985ad613c087
codewithtolu463-oss <codewithtolu463@gmail.com>
2026-05-06 02:35:10 +1200

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
 ob_start();
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
+
 header("Access-Control-Allow-Origin: https://focusdeskk.netlify.app");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
