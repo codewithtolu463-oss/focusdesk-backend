42cfdd6d60e1a92c275dc8b642bc2c783216b155
codewithtolu463-oss <codewithtolu463@gmail.com>
2026-05-10 15:45:58 +1200

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,12 @@
 <?php
 ob_start();
 
-header("Access-Control-Allow-Origin: https://focusdeskk.netlify.app");
+$allowed = ['https://focusdeskk.netlify.app', 'http://localhost:5173'];
+$origin = $_SERVER['HTTP_ORIGIN'] ?? '';
+if(in_array($origin, $allowed)){
+    header("Access-Control-Allow-Origin: $origin");
+}
+
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header("Content-Type: application/json");
