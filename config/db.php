ab0c74da70e79c362bbdcf1bc60a69b216a9b047
codewithtolu463-oss <codewithtolu463@gmail.com>
2026-05-26 13:41:17 +1200

    Update db.php
    chnged the password

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 error_log("URI: " . $_SERVER['REQUEST_URI'] . " METHOD: " . $_SERVER['REQUEST_METHOD']);
 $host = getenv('bcaraw5zsuiq512xifvc-mysql.services.clever-cloud.com');
 $username = getenv('uc7besw319b7eviy');
-$password = getenv('nuuAMMBNK5Ybw905LNcO');
+$password = getenv('VGw583xGjoVq5x8Z2J6V');
 $database_name = getenv('bcaraw5zsuiq512xifvc');
 $port = getenv('3306');
 $conn = new mysqli($host, $username, $password, $database_name, $port);
