9d921b0e9e8880047a0962e235b402343c346bdc
codewithtolu463-oss <codewithtolu463@gmail.com>
2026-05-26 13:10:15 +1200

    Update db.php

--- config/db.php
+++ config/db.php
@@ -1,10 +1,10 @@
 <?php
 error_log("URI: " . $_SERVER['REQUEST_URI'] . " METHOD: " . $_SERVER['REQUEST_METHOD']);
-$host = getenv('MYSQLHOST');
-$username = getenv('MYSQLUSER');
-$password = getenv('MYSQLPASSWORD');
-$database_name = getenv('MYSQLDATABASE');
-$port = getenv('MYSQLPORT');
+$host = getenv('bcaraw5zsuiq512xifvc-mysql.services.clever-cloud.com');
+$username = getenv('uc7besw319b7eviy');
+$password = getenv('nuuAMMBNK5Ybw905LNcO');
+$database_name = getenv('bcaraw5zsuiq512xifvc');
+$port = getenv('3306');
 $conn = new mysqli($host, $username, $password, $database_name, $port);
 if($conn->connect_error){
     die(json_encode(['message' => 'Connection failed: ' . $conn->connect_error]));
